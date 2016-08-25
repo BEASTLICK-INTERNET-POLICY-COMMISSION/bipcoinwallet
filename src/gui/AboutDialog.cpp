19525f7a26b30dd9b9f80d262baf7e906a962d54
Derrick <derrick@alienseed.com>
2016-08-25 09:44:13 -0400

    About box  Minor text updates  Show version in windows  Fix bipcoin.org link Rename cryptonote.rc to bipcoin.rc

--- src/gui/AboutDialog.cpp
+++ src/gui/AboutDialog.cpp
@@ -5,6 +5,7 @@
 #include "AboutDialog.h"
 #include "CurrencyAdapter.h"
 #include "Settings.h"
+#include "version.h"
 
 #include "ui_aboutdialog.h"
 
@@ -14,7 +15,7 @@ AboutDialog::AboutDialog(QWidget* _parent) : QDialog(_parent), m_ui(new Ui::Abou
   m_ui->setupUi(this);
   setWindowTitle(QString(tr("About %1 Wallet")).arg(CurrencyAdapter::instance().getCurrencyDisplayName()));
   QString aboutText = m_ui->m_aboutLabel->text();
-  m_ui->m_aboutLabel->setText(aboutText.arg(GIT_REVISION));
+  m_ui->m_aboutLabel->setText(aboutText.arg(BUILD_COMMIT_ID));
 }
 
 AboutDialog::~AboutDialog() {
